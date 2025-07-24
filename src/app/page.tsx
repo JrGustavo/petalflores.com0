40ed6c3120e616ce10ddcfa782464b49b5b2fe6c
JrGustavo <gadolfoalvarado@gmail.com>
2025-07-23 21:56:11 -0500

    Update PopUp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import Reminder from "@/app/(LandingPage)/Reminder";
 import TopSellers from "./(LandingPage)/TopSellers";
 import Impacto from "@/app/(LandingPage)/Impacto";
 import Reseñas from "@/app/(LandingPage)/Reseñas";
+import PopUp from "@/app/(LandingPage)/Pop-up";
 
 export default function Home() {
   return (
@@ -22,6 +23,7 @@ export default function Home() {
   <Impacto/>
   <Reseñas/>
   <Footer/>
+  <PopUp/>
 
     </div>
   );

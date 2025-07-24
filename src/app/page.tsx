3bcf57521c145ea567b650d86cd6e3760aff3e85
JrGustavo <gadolfoalvarado@gmail.com>
2025-07-23 21:58:28 -0500

    Delete Page popup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ import Reminder from "@/app/(LandingPage)/Reminder";
 import TopSellers from "./(LandingPage)/TopSellers";
 import Impacto from "@/app/(LandingPage)/Impacto";
 import Reseñas from "@/app/(LandingPage)/Reseñas";
-import PopUp from "@/app/(LandingPage)/Pop-up";
 
 export default function Home() {
   return (
